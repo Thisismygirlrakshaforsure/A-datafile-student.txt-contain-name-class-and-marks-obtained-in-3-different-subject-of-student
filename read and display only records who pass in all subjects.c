96ce080260f6808b1d04f4fb6993fd763f3ed647
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-04-06 07:51:12 +0545

    Update read and display only records who pass in all subjects.c

--- read and display only records who pass in all subjects.c
+++ read and display only records who pass in all subjects.c
@@ -7,7 +7,7 @@ int main()
     fptr = fopen("student.txt","r");
     while(fscanf(fptr,"%s %d %d %d %d",n,&c,&e,&ne,&m) != EOF)
     {      
-       if (e>=40 && ne>=40 && m>=40)
+       if (e>=27 && ne>=27 && m>=27)
         {
           printf("%s %d  %d  %d  %d", n, c, e, ne, m);    
         }
